babd06f718729ac5d86750d8e8a58ed6be3b0940
Dang Mai <contact@dangmai.net>
2018-03-04 17:13:45 -0500

    Fix env var not being used by cron

--- tarsnapper-backup.sh
+++ tarsnapper-backup.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
-BACKUP_COMMAND=${BACKUP_COMMAND:-"-c /etc/tarsnapper.conf make"}
 export PATH=$PATH:/usr/local/bin
 tarsnap --fsck && tarsnapper $BACKUP_COMMAND
--- tarsnapper-expire.sh
+++ tarsnapper-expire.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
-EXPIRE_COMMAND=${EXPIRE_COMMAND:-"-c /etc/tarsnapper.conf expire"}
 export PATH=$PATH:/usr/local/bin
 tarsnap --fsck && tarsnapper $EXPIRE_COMMAND
